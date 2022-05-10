f35ee05f532d28c54afb61016b8d737ada5bdc6f
Madhukar Yerraguntla <madhukar.yerraguntla@gmail.com>
2022-05-10 11:47:30 +0530

    writing the nom parser

--- codegen/src/inst_parser.rs
+++ codegen/src/inst_parser.rs
@@ -1,5 +1,8 @@
-use nom::{
-    IResult,
-};
+use nom::{IResult, error::VerboseError};
+use nom::number::complete::be_u16;
+use nom::bytes::complete::take;
 
 
+
+use crate::options::{Instruction, Time};
+type Res<T,U> = IResult<T, U, VerboseError<T>>;

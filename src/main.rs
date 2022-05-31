d5da17eaded4f749f60f71843b38b1d91097f76e
Madhukar Yerraguntla <rathnakar.y@nxp.com>
2022-05-31 15:11:31 +0530

    testing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.len() > b.len() {
+        return a
+    }
+    else {
+        b
+    }
+
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+    
 }
